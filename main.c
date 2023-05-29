60baf86ef06326aab713a0a3de225c4c4be0fb34
ticma15 <134615256+ticma15@users.noreply.github.com>
2023-05-29 14:33:16 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,120 +1,185 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-//gitbash
+#include <string.h>
+#include <errno.h>
 
-//3 
-typedef struct clan {
-	char ime[50];
-	char prezime[50];
-	int id;
+#define MAX_NAME_LENGTH 50 //12
+
+//Odradene tocke: 1, 2, 3, 4, 8, 9, 10, 12, 13, 14, 16, 17, 18, 19
+//Ukupan broj odradenih tocaka: 14
+
+
+typedef struct clan { //3, 2
+    char* ime;      //1
+    char* prezime;
+    int id;
 } CLAN;
 
-void dodajClana(FILE* file) {
-	CLAN clan;
+void dodajClana(FILE* file) { //4
+    CLAN clan;
+    clan.ime = malloc(MAX_NAME_LENGTH * sizeof(char));
+    clan.prezime = malloc(MAX_NAME_LENGTH * sizeof(char));
 
-	printf("Unesi podatke clana:\n");
-	printf("Ime: ");
-	scanf("%s", clan.ime);
-	printf("Prezime: ");
-	scanf("%s", clan.prezime);
-	printf("ID: ");
-	scanf("%d", &clan.id);
+    printf("Unesi podatke clana:\n");
+    printf("Ime: ");
+    scanf("%s", clan.ime);
+    printf("Prezime: ");
+    scanf("%s", clan.prezime);
 
-	fprintf(file, "%s %s %d\n", clan.ime, clan.prezime, clan.id);
-	printf("Clan je uspjesno dodan.\n");
+    int result = 0;
+    do {
+        printf("ID: ");
+        result = scanf("%d", &clan.id);
+        if (result != 1) {
+            printf("Neispravan unos za ID. Molimo unesite ponovno.\n");
+            while (fgetc(stdin) != '\n');
+        }
+    } while (result != 1);
+
+    fprintf(file, "%s %s %d\n", clan.ime, clan.prezime, clan.id);
+    printf("Clan je uspjesno dodan.\n");
+
+    free(clan.ime);
+    free(clan.prezime);
 }
 
-void izbrisiClana(FILE* file, int id) { //4
-	FILE* tf = fopen("temp.txt", "w");
-	CLAN clan;
-	int pronaden = 0;
+void izbrisiClana(FILE* file, int id) {
+    FILE* tf = fopen("temp.txt", "w");
+    if (tf == NULL) {
+        perror("Greska pri otvaranju temp.txt");
+        return;
+    }
 
-	while (fscanf(file, "%s %s %d", clan.ime, clan.prezime, &clan.id) != EOF) {
-		if (clan.id == id) {
-			pronaden = 1;
-			continue;
-		}
+    CLAN clan;
+    clan.ime = malloc(MAX_NAME_LENGTH * sizeof(char));
+    clan.prezime = malloc(MAX_NAME_LENGTH * sizeof(char));
 
-		fprintf(tf, "%s %s %d\n", clan.ime, clan.prezime, clan.id);
-	}
+    int pronaden = 0;
 
-	fclose(file);
-	fclose(tf);
+    while (fscanf(file, "%s %s %d", clan.ime, clan.prezime, &clan.id) != EOF) {
+        if (clan.id == id) {
+            pronaden = 1;
+            continue;
+        }
 
-	if (pronaden) {
-		remove("gym.txt");	//17
-		rename("temp.txt", "gym.txt");
-		printf("Clan je izbrisan.\n");
-	}
-	else {
-		remove("temp.txt");
-		printf("Clan nije pronaden.\n");
-	}
+        fprintf(tf, "%s %s %d\n", clan.ime, clan.prezime, clan.id);
+    }
 
-	
-	file = fopen("gym.txt", "a+");
+    fclose(file);
+    fclose(tf);
+
+    if (pronaden) {
+        if (remove("gym.txt") != 0) {
+            perror("Greska pri brisanju gym.txt"); //19
+            return;
+        }
+
+        if (rename("temp.txt", "gym.txt") != 0) {
+            perror("Greska pri preimenovanju temp.txt");
+            return;
+        }
+
+        printf("Clan je izbrisan.\n");
+    }
+    else {
+        remove("temp.txt"); //18
+        printf("Clan nije pronaden.\n");
+    }
+
+    file = fopen("gym.txt", "a+");
+    if (file == NULL) {
+        perror("Greska pri otvaranju gym.txt");
+    }
+
+    free(clan.ime);
+    free(clan.prezime);
 }
 
-void ispisClanova(FILE* file) {
-	CLAN clan;
+void ispisClanova(FILE* file) { //9, 10
+    CLAN clan;
+    clan.ime = malloc(MAX_NAME_LENGTH * sizeof(char)); //13, 14
+    clan.prezime = malloc(MAX_NAME_LENGTH * sizeof(char));
 
-	printf("Clanovi teretane:\n");
-	while (fscanf(file, "%s %s %d", clan.ime, clan.prezime, &clan.id) != EOF) {
-		printf("Ime: %s %s\n", clan.ime, clan.prezime);
-		printf("ID: %d\n\n", clan.id);
-	}
+    printf("Clanovi teretane:\n");
+    while (fscanf(file, "%s %s %d", clan.ime, clan.prezime, &clan.id) != EOF) {
+        printf("Ime: %s %s\n", clan.ime, clan.prezime);
+        printf("ID: %d\n\n", clan.id);
+    }
 
-	fclose(file);
-	file = fopen("gym.txt", "a+");
+    fclose(file); 
+    file = fopen("gym.txt", "a+"); //16
+    if (file == NULL) {
+        perror("Greska pri otvaranju gym.txt");
+    }
+
+    free(clan.ime);
+    free(clan.prezime);
 }
 
 int main(void) {
-	FILE* file = fopen("gym.txt", "a+");
+    FILE* file = fopen("gym.txt", "a+");
+    if (file == NULL) {
+        perror("Greska pri otvaranju datoteke");
+        return 1;
+    }
 
-	if (file == NULL) {
-		printf("Greska pri otvaranju datoteke.\n");
-		return 1;
-	}
+    int odabir, id;
 
-	int odabir, id;
+    do {
+        printf("Izbornik:\n"); //8
+        printf("1. Dodaj clanove\n");
+        printf("2. Izbrisi clanove\n");
+        printf("3. Ispisi listu clanova\n");
+        printf("4. Izlaz\n");
+        printf("Tvoj odabir: ");
 
-	do {
-		printf("Izbornik:\n"); //8
-		printf("1. Dodaj clanove\n");
-		printf("2. Izbrisi clanove\n");
-		printf("3. Ispisi listu clanova\n");
-		printf("4. Izlaz\n");
-		printf("Tvoj odabir: ");
-		scanf("%d", &odabir);
-		printf("\n");
+        int result = 0;
+        do {
+            result = scanf("%d", &odabir);
+            if (result != 1) {
+                printf("Neispravan unos. Molimo unesite ponovno.\n");
+                while (fgetc(stdin) != '\n');
+            }
+        } while (result != 1);
 
-		switch (odabir) {
-		case 1:
-			dodajClana(file);
-			break;
-		case 2:
-			printf("Upisi ID clana kojeg zelite izbrisati: ");
-			scanf("%d", &id);
-			izbrisiClana(file, id);
-			break;
-		case 3:
-			fseek(file, 0, SEEK_SET);  //17
-			ispisClanova(file);
-			break;
-		case 4:
-			printf("Izlazak iz programa.\n");
-			break;
-		default:
-			printf("Pogresan unos. Pokusaj ponovo.\n");
-			break;
-		}
+        printf("\n");
 
-		printf("\n");
+        switch (odabir) {
+        case 1:
+            dodajClana(file);
+            break;
+        case 2:
+            printf("Upisi ID clana kojeg zelite izbrisati: ");
 
-	} while (odabir != 4);
+            result = 0;
+            do {
+                result = scanf("%d", &id);
+                if (result != 1) {
+                    printf("Neispravan unos za ID. Molimo unesite ponovno.\n");
+                    while (fgetc(stdin) != '\n');
+                }
+            } while (result != 1);
 
-	fclose(file);
+            izbrisiClana(file, id);
+            break;
+        case 3:
+            fseek(file, 0, SEEK_SET); //17
+            ispisClanova(file);
+            break;
+        case 4:
+            printf("Izlazak iz programa.\n");
+            break;
+        default:
+            printf("Pogresan unos. Molimo unesite ponovno.\n");
+            break;
+        }
 
-	return 0;
+        printf("\n");
+
+    } while (odabir != 4);
+
+    fclose(file);
+
+    return 0;
 }
